c14ea3797cb02963efabad00207e55ca9019d389
Corey J. Nolet <cjnolet@gmail.com>
2020-07-03 18:41:58 -0400

    Checking profiling pytests in

--- tests/cupyx_tests/scipy_tests/sparse_tests/test_index.py
+++ tests/cupyx_tests/scipy_tests/sparse_tests/test_index.py
@@ -7,11 +7,11 @@ import pytest
 
 
 @testing.parameterize(*testing.product({
-    'format': ['csr', 'csc'],
-    'density': [0.1, 0.2, 0.4, 0.5, 0.9, 1.0],
+    'format': ['csc', 'csr'],
+    'density': [0.9],
     'dtype': ['float32', 'float64', 'complex64', 'complex128'],
-    'n_rows': [100, 1000],
-    'n_cols': [10, 100]
+    'n_rows': [100000],
+    'n_cols': [1000]
 }))
 @testing.with_requires('scipy')
 class TestIndexing(unittest.TestCase):
@@ -21,186 +21,68 @@ class TestIndexing(unittest.TestCase):
                                format=self.format,
                                density=self.density)
 
+        print(self.format)
+        print(self.dtype)
+
         # sparse.random doesn't support complex types
         # so we need to cast
         a = a.astype(self.dtype)
         if min is not None:
-            expected = a.get()[maj, min]
-            actual = a[maj, min]
+            expected = a.get()
+
+            with cupy.prof.time_range(message="cpu", color_id=1):
+                import time
+                start_cpu = time.time()
+                expected = expected[maj, min]
+                print("cpu_time: %s" % (time.time()-start_cpu))
+
+            with cupy.prof.time_range(message="gpu", color_id=2, sync=True):
+                start_gpu = time.time()
+                actual = a[maj, min]
+                print("gpu_time: %s" % (time.time() - start_gpu))
         else:
-            expected = a.get()[maj]
-            actual = a[maj]
+            expected = a.get()
+
+            import time
+            start_cpu = time.time()
+            with cupy.prof.time_range(message="cpu", color_id=1):
+                expected = expected[maj]
+            print("cpu_time: %s" % (time.time()-start_cpu))
+
+            start_gpu = time.time()
+            with cupy.prof.time_range(message="gpu", sync=True, color_id=2):
+                actual = a[maj]
+            print("gpu_time: %s" % (time.time() - start_gpu))
 
         if cupy.sparse.isspmatrix(actual):
-            actual = actual.todense()
-            expected = expected.todense()
+            actual.sort_indices()
+            expected.sort_indices()
 
-        cupy.testing.assert_array_equal(actual.ravel(),
-                                        cupy.array(expected).ravel())
+            cupy.testing.assert_array_equal(actual.indptr,
+                                            cupy.array(expected.indptr))
 
-    def test_major_slice(self):
-        self._run(slice(5, 9))
-        self._run(slice(9, 5))
+            cupy.testing.assert_array_equal(actual.indices,
+                                            cupy.array(expected.indices))
 
-    def test_major_all(self):
-        self._run(slice(None))
+            cupy.testing.assert_array_equal(actual.data,
+                                            cupy.array(expected.data))
 
-    def test_major_scalar(self):
-        self._run(10)
+        else:
+            cupy.testing.assert_array_equal(actual.ravel(),
+                                            cupy.array(expected).ravel())
+
+
+    import cupy.prof
 
     def test_major_fancy(self):
-        self._run([1, 5, 4])
-        self._run([10, 2])
-        self._run([2])
 
-    def test_major_slice_minor_slice(self):
-        self._run(slice(1, 5), slice(1, 5))
+        import numpy
 
-    def test_major_slice_minor_all(self):
-        self._run(slice(1, 5), slice(None))
-        self._run(slice(5, 1), slice(None))
+        r = numpy.arange(self.n_cols)
+        m = numpy.arange(self.n_rows)
 
-    def test_major_slice_minor_scalar(self):
-        self._run(slice(1, 5), 5)
-        self._run(slice(5, 1), 5)
-        self._run(slice(5, 1, -1), 5)
+        c = cupy.asarray(numpy.random.choice(r, size=100))
+        n = cupy.asarray(numpy.random.choice(m, size=100)).astype("int32")
 
-    def test_major_slice_minor_fancy(self):
-        self._run(slice(1, 10, 2), [1, 5, 4])
-
-    def test_major_scalar_minor_slice(self):
-        self._run(5, slice(1, 5))
-
-    def test_major_scalar_minor_all(self):
-        self._run(5, slice(None))
-
-    def test_major_scalar_minor_scalar(self):
-        self._run(5, 5)
-
-    def test_major_scalar_minor_fancy(self):
-        self._run(5, [1, 5, 4])
-
-    def test_major_all_minor_scalar(self):
-        self._run(slice(None), 5)
-
-    def test_major_all_minor_slice(self):
-        self._run(slice(None), slice(5, 10))
-
-    def test_major_all_minor_all(self):
-        self._run(slice(None), slice(None))
-
-    def test_major_all_minor_fancy(self):
-        self._run(slice(None), [1, 5, 4])
-
-    def test_major_fancy_minor_fancy(self):
-        self._run([1, 5, 4], [1, 5, 4])
-        self._run([2, 0, 10], [9, 2, 1])
-        self._run([2, 0], [2, 1])
-
-    def test_major_fancy_minor_all(self):
-        self._run([1, 5, 4], slice(None))
-
-    def test_major_fancy_minor_scalar(self):
-        self._run([1, 5, 4], 5)
-
-    def test_major_fancy_minor_slice(self):
-        self._run([1, 5, 4], slice(1, 5))
-        self._run([1, 5, 4], slice(5, 1, -1))
-
-    def test_major_bool_fancy(self):
-        rand_bool = cupy.random.random(self.n_rows).astype(cupy.bool)
-        self._run(rand_bool.get())
-
-    def test_major_slice_with_step(self):
-
-        # positive step
-        self._run(slice(1, 10, 2))
-        self._run(slice(2, 10, 5))
-        self._run(slice(0, 10, 10))
-
-        self._run(slice(1, None, 2))
-        self._run(slice(2, None, 5))
-        self._run(slice(0, None,  10))
-
-        # negative step
-        self._run(slice(10, 1, -2))
-        self._run(slice(10, 2, -5))
-        self._run(slice(10, 0, -10))
-
-        self._run(slice(10, None, -2))
-        self._run(slice(10, None, -5))
-        self._run(slice(10, None, -10))
-
-    def test_major_slice_with_step_minor_slice_with_step(self):
-
-        # positive step
-        self._run(slice(1, 10, 2), slice(1, 10, 2))
-        self._run(slice(2, 10, 5), slice(2, 10, 5))
-        self._run(slice(0, 10, 10), slice(0, 10, 10))
-
-        # negative step
-        self._run(slice(10, 1, 2), slice(10, 1, 2))
-        self._run(slice(10, 2, 5), slice(10, 2, 5))
-        self._run(slice(10, 0, 10), slice(10, 0, 10))
-
-    def test_major_slice_with_step_minor_all(self):
-
-        # positive step
-        self._run(slice(1, 10, 2), slice(None))
-        self._run(slice(2, 10, 5), slice(None))
-        self._run(slice(0, 10, 10), slice(None))
-
-        # negative step
-        self._run(slice(10, 1, 2), slice(None))
-        self._run(slice(10, 2, 5), slice(None))
-        self._run(slice(10, 0, 10), slice(None))
-
-    def test_major_all_minor_slice_step(self):
-
-        # positive step incr
-        self._run(slice(None), slice(1, 10, 2))
-        self._run(slice(None), slice(2, 10, 5))
-        self._run(slice(None), slice(0, 10, 10))
-
-        # positive step decr
-        self._run(slice(None), slice(10, 1, 2))
-        self._run(slice(None), slice(10, 2, 5))
-        self._run(slice(None), slice(10, 0, 10))
-
-        # positive step incr
-        self._run(slice(None), slice(10, 1, 2))
-        self._run(slice(None), slice(10, 2, 5))
-        self._run(slice(None), slice(10, 0, 10))
-
-        # negative step decr
-        self._run(slice(None), slice(10, 1, -2))
-        self._run(slice(None), slice(10, 2, -5))
-        self._run(slice(None), slice(10, 0, -10))
-
-    def test_major_reorder(self):
-        self._run(slice(None, None, -1))
-        self._run(slice(None, None, -2))
-        self._run(slice(None, None, -50))
-
-    def test_major_reorder_minor_reorder(self):
-        self._run(slice(None, None, -1), slice(None, None, -1))
-        self._run(slice(None, None, -3), slice(None, None, -3))
-
-    def test_ellipsis(self):
-        self._run(Ellipsis)
-        self._run(Ellipsis, 1)
-        self._run(1, Ellipsis)
-        self._run(Ellipsis, slice(None))
-        self._run(slice(None), Ellipsis)
-        self._run(Ellipsis, slice(1, None))
-        self._run(slice(1, None), Ellipsis)
-
-    def test_bad_indexing(self):
-        with pytest.raises(IndexError):
-            self._run("foo")
-
-        with pytest.raises(IndexError):
-            self._run(2, "foo")
-
-        with pytest.raises(ValueError):
-            self._run([1, 2, 3], [1, 2, 3, 4])
+        with cupy.prof.time_range(message="test_major_fancy. format=%s, dtype=%s" % (self.format, self.dtype)):
+            self._run(slice(50, 500))
